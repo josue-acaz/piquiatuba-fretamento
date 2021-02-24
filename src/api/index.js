10fd5a36724063ad969511c9852fa18933a55484
Piquiatuba Taxi Aereo <piquiatuba@MacBook-Air-de-Piquiatuba.local>
2021-02-24 20:23:49 -0300

    registro.br

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import { baseURL } from '../global';
 import { authHeader } from '../auth/helpers/auth-header';
 
 const api = axios.create({
-    baseURL: 'http://144.126.221.7:3333',
+    baseURL: 'https://api.charterpiquiatuba.com.br',
 });
 
 api.interceptors.request.use(function (config) {
