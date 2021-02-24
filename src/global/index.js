e22a31c9fc542f95a14dd8f28d410cd71ebef818
Piquiatuba Taxi Aereo <piquiatuba@MacBook-Air-de-Piquiatuba.local>
2021-02-24 20:48:54 -0300

    test

--- src/global/index.js
+++ src/global/index.js
@@ -1,5 +1,5 @@
 const PDF_URL = process.env.REACT_APP_AWS_CLOUD+'/public/pdfs';
-const baseURL = 'http://144.126.221.7:3333';
+const baseURL = "https://api.charterpiquiatuba.com.br";
 
 const EnumAircraftType = [
     {
