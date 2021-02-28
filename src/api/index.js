1476caa3771d3e5261caf0e7c2e6f2d9dec01a04
Josué Acaz <josue.acaz@outlook.com>
2021-02-28 16:58:34 -0300

    ajustes

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,7 @@ import { authHeader } from '../auth/helpers/auth-header';
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
 });
+
 //https://api.charterpiquiatuba.com.br process.env.REACT_APP_API_URL
 api.interceptors.request.use(function (config) {
     config.headers = {
